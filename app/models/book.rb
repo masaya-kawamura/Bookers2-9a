8b44501355e2014a08a8bdaedfac06ebe2e730d4
masaya-kawamura <lucky.number0128@gmail.com>
2021-06-05 04:05:09 +0000

    [開始]Bookersの投稿の閲覧数機能を追加

---
+++ app/models/book.rb
@@ -0,0 +1,29 @@
+class Book < ApplicationRecord
+	belongs_to :user
+	has_many :favorites, dependent: :destroy
+	has_many :comments,  dependent: :destroy
+	has_many :favorite_users, through: :favorites, source: :user
+
+	def favorited_by?(user)
+		favorites.where(user_id: user.id).exists?
+	end
+
+	validates :title, presence: true
+	validates :body, presence: true, length: {maximum: 200}
+
+	# 検索機能のlooksメソッド定義
+	def self.looks(search,word)
+		if search == 'perfect_match'
+			@book = Book.where('title LIKE?', "#{word}")
+		elsif search == 'forword_match'
+			@book = Book.where('title LIKE?', "#{word}%")
+		elsif search == 'backword_match'
+			@book = Book.where('title LIKE?', "%#{word}")
+		elsif search == 'partial_match'
+			@book = Book.where('title LIKE?', "%#{word}%")
+		else
+			@book = Book.all
+		end
+	end
+
+end
